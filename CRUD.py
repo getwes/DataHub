3581a7731ff4e141ccb58213138a6fff429634e6
unknown <wesleyfreire1707@gmail.com>
2024-11-20 11:37:27 -0300

    Término Da Alteração Dos Dados Do Cliente

--- CRUD.py
+++ CRUD.py
@@ -18,16 +18,15 @@ comando = f'INSERT INTO clientes(nome, email, idade, senha) VALUES ("{nome}", "{
 cursor.execute(comando)
 conexao.commit()
 
-valor = input("senhor precisa editar os dados ? sim / nao: ")
-if valor == "sim":
+valor = input("senhor precisa editar os dados ? s / n: ")
+if valor == "s":
     print("ok")
-    dados = input("digite seu email aqui:")
-    if dados == email:
-     print("\nO que você deseja editar?")
+    print("\nO que você deseja editar?")
     print("1. Nome")
     print("2. Email")
     print("3. Idade")
     print("4. Senha")
+
     opcao = input("Escolha uma opção (1/2/3/4): ")
 
     if opcao == "1":
@@ -38,14 +37,34 @@ if valor == "sim":
 
         print(f"seu nome foi trocado para {novo_nome}")
 
-    if opcao == "2":
+    elif opcao == "2":
         novo_email = input("digite seu novo email: ")
         comando = f'UPDATE clientes SET email = "{novo_email}" where email = "{email}"'
         cursor.execute(comando)
         conexao.commit()
 
         print(f"seu novo email é {novo_email}")
-  
+
+    elif opcao == "3":
+        nova_idade = input("digite sua nova idade: ")
+        comando = f'UPDATE clientes SET idade = {nova_idade} where idade = "{idade}"'
+        cursor.execute(comando)
+        conexao.commit()
+
+        print(f"sua nova idade é {nova_idade}")
+
+    elif opcao == "4":
+        nova_senha = input("digite sua nova senha: ")
+        comando = f'UPDATE clientes SET senha = {nova_senha} where senha = "{senha}"'
+        cursor.execute(comando)
+        conexao.commit()
+
+        print(f"sua nova idade é {nova_senha}")
+
+else:
+    valor == "n"
+    print(f"ok senhor/a {nome} seu cadastro foi concluido ")
+
 
 cursor.close()
 conexao.close()
